82bb180e1ec1818a41f35b16604957922b1baa41
Ram Chapagai <ramchapagai@Rams-MBP.attlocal.net>
2024-04-27 15:28:37 -0400

    feat(): removed animation

--- ui/resume-project/src/components/Home.jsx
+++ ui/resume-project/src/components/Home.jsx
@@ -1,33 +1,9 @@
-import { useState, useEffect } from 'react';
 import profileImage from '../assets/profile.png'; // Import your profile image
 import ParticleComponent from './ParticleComponent';
 import './Home.css'; // Import your CSS file
 import SocialLinks from './SocialLinks';
 
 const Home = () => {
-
-  const [randomColor, setRandomColor] = useState(''); // State to hold random color
-
-  // Function to generate random RGB color
-  const getRandomColor = () => {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`;
-  };
-
-  useEffect(() => {
-    // Set random color when component mounts
-    setRandomColor(getRandomColor());
-
-    // Change color during easing out
-    const timeout = setTimeout(() => {
-      document.querySelector('.greeting').style.color = getRandomColor(); // Change color dynamically
-      document.querySelector('.greeting').classList.add('fade-out');
-    }, 1500); // Adjust the timing to match the easing out duration
-
-    return () => clearTimeout(timeout);
-  }, []);
 
   const greeting = new Date().getHours() < 12 ? 'Good Morning' : 'Good Afternoon';
 
@@ -36,7 +12,7 @@ const Home = () => {
     <ParticleComponent/>
      <div className="home-container">
       <div className="greeting-container">
-        <h1 className="greeting" style={{ color: randomColor }}>👋🏻 {greeting}</h1>
+        <h1 className="greeting">👋🏻 {greeting}</h1>
         <p>
           Hi, I am  Ram ! I am a passionate Software Engineer with experience in AWS and Web Development. Welcome to my portfolio
           website. Here, you can learn more about my background, skills, and projects.
